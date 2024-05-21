d358067f5c5fd8b6108c657f7b8d0bbcdc6c25b0
choihansu97 <jjpp9712@naver.com>
2024-05-21 11:25:07 +0900

    fix(deploy): 재배포

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#"><span> 드림렌트카 </span><span style="font-size: 15px"> 견적계산기</span></a>
+            <a class="navbar-brand" href="#"><span>벨류렌터카</span><span style="font-size: 15px"> 견적계산기</span></a>
         </div>
     </nav>
 </template>
