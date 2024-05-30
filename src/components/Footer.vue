e4d06eabb8d6d5768b15fad0693abd14f2cef941
39192 <160730385+39192@users.noreply.github.com>
2024-05-30 09:37:22 +0900

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 <template>
     <footer class="py-3 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright © 벨류렌터카 2024</p>
+            <p class="m-0 text-center text-white">Copyright © 괌자길 2024</p>
         </div>
     </footer>
 </template>
